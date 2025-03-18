ef5ba4bf64155e05ee5e1eebcd7039c54470e32e
ZeroRocks <94911198+ZeroR0cks@users.noreply.github.com>
2025-03-18 19:42:06 +0500

    Delete src directory

--- src/main/java/edu/phystech/hw2/analyzer/SpamAnalyzer.java
+++
@@ -1,9 +0,0 @@
-package edu.phystech.hw2.analyzer;
-
-import java.util.List;
-
-public class SpamAnalyzer extends KeywordAnalyzer{
-    public SpamAnalyzer(List<String> keywords) {
-        super(null, null);
-    }
-}
--- src/main/java/edu/phystech/hw5/exception/ValidationException.java
+++
@@ -1,7 +0,0 @@
-package edu.phystech.hw5.exception;
-
-/**
- * @author kzlv4natoly
- */
-public class ValidationException extends RuntimeException {
-}
