43d273bc82aaf8ffe4159ca54a10a4d1fec32dca
ZeroRocks <94911198+ZeroR0cks@users.noreply.github.com>
2025-03-18 19:51:56 +0500

    Delete src directory

--- src/main/java/edu/phystech/hw2/analyzer/Label.java
+++
@@ -1,6 +0,0 @@
-package edu.phystech.hw2.analyzer;
-
-
-public enum Label {
-    SPAM, NEGATIVE, TOO_LONG, OK
-}
--- src/main/java/edu/phystech/hw5/service/Validator.java
+++
@@ -1,8 +0,0 @@
-package edu.phystech.hw5.service;
-
-/**
- * @author kzlv4natoly
- */
-public interface Validator {
-    void validate(Object object);
-}
